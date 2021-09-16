43959611a9ec4c4b95cd8e6f3b0f1049cf91f23e
Aaron <cherongwa@gmail.com>
2021-09-16 19:08:20 -0400

    DAY TWO

---
+++ src/CLASS1/TEST.java
@@ -0,0 +1,15 @@
+package CLASS1;
+
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+public class TEST {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b =40;
+        int c = a+b;
+      System.out.println("hello java");
+        System.out.println("getting interesting");
+        System.out.println(c);
+    }
+}
